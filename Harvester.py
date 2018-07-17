8bd920900bb0e1523da221f5b2a59d5fbe8e40a9
Leo Monus <u4187959@ands-devl.anu.edu.au>
2018-07-17 11:46:00 +1000

    add RedisPoster's host to config

--- Harvester.py
+++ Harvester.py
@@ -65,11 +65,14 @@ class RedisPoster:
 
     poster = False
 
+
     def __init__(self):
-        self.poster = redis.StrictRedis(host='localhost', port=6379, db=0)
+        if myconfig.redis_poster_host != "":
+            self.poster = redis.StrictRedis(host=myconfig.redis_poster_host, port=6379, db=0)
 
     def postMesage(self, chanel, message):
-        self.poster.publish(chanel, message)
+        if self.poster:
+            self.poster.publish(chanel, message)
 
 
 class XSLT2Transformer:
