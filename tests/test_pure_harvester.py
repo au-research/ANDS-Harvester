49d507678807da61c0d909633a3d82bd5fbe8b38
Leo Monus <leo.monus@anu.edu.au>
2019-09-11 14:54:50 +1000

    crosswalk only harvest test

--- tests/test_pure_harvester.py
+++ tests/test_pure_harvester.py
@@ -60,6 +60,8 @@ class test_pure_harvester(unittest.TestCase):
 
     @patch.object(Request, 'getData')
     def test_uwa_pure_crosswalk_only(self, mockGetData):
+        batch_id = "PURE_UWA"
+        ds_id = 1
         mockGetData.side_effect = [
             self.readTestfile('page_1.xml'),
             self.readTestfile('page_2.xml'),
@@ -72,11 +74,11 @@ class test_pure_harvester(unittest.TestCase):
         harvestInfo['harvest_method'] = 'PUREHarvester'
         harvestInfo['data_store_path'] = myconfig.data_store_path
         harvestInfo['response_url'] = myconfig.response_url
-        harvestInfo['data_source_id'] = 7
+        harvestInfo['data_source_id'] = ds_id
         harvestInfo['harvest_id'] = 1
-        harvestInfo['batch_number'] = "PURE_UWA"
+        harvestInfo['batch_number'] = batch_id
         harvestInfo['advanced_harvest_mode'] = "STANDARD"
-        harvestInfo['xsl_file'] = myconfig.abs_path + "/resources/schemadotorg2rif.xsl"
+        harvestInfo['xsl_file'] = myconfig.abs_path + "/tests/resources/xslt/Elsevier_PURE_Params.xsl"
         harvestInfo['mode'] = "TEST"
         harvestInfo['api_key'] = myconfig.uwa_api_key
         # harvestReq = JSONLDHarvester.JSONLDHarvester(harvestInfo)
@@ -84,6 +86,14 @@ class test_pure_harvester(unittest.TestCase):
         # t.start()
         harvester = PUREHarvester(harvestInfo)
         harvester.crosswalk()
+        tempFile = myconfig.data_store_path + str(ds_id) + os.sep + batch_id + os.sep + "1.tmp"
+        resultFile = myconfig.data_store_path + str(ds_id) + os.sep + batch_id + os.sep + "1.xml"
+        self.assertTrue(os.path.exists(tempFile))
+        self.assertTrue(os.path.exists(resultFile))
+        content = self.readFile(tempFile)
+        self.assertIn('<doi>10.4225/57/5b29e9c09280e</doi>', content)
+        content = self.readFile(resultFile)
+        self.assertIn('<identifier type="doi">http://doi.org/10.4225/57/5b29e9c09280e</identifier>', content)
 
     # API KEYS PROVIDED BY Melanie (for testing)
     def only_during_developement_test_uwa_pure_external(self):
