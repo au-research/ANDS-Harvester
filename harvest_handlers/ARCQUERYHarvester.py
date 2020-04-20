7fe66d4fcbb69e3c6f660e4cb0bd6cb1cb92713e
Liz Woods <lwoods@ANDSs-MBP.lan>
2020-04-20 15:25:40 +1000

    Added iteration of call to api to obtain the grant ids before individual call for id and added apppropraite files for mock of getData RDA-85

--- harvest_handlers/ARCQUERYHarvester.py
+++ harvest_handlers/ARCQUERYHarvester.py
@@ -1,6 +1,7 @@
 from Harvester import *
 import urllib
 import urllib.parse as urlparse
+import array as arr
 class ARCQUERYHarvester(Harvester):
     """
        {
@@ -15,6 +16,7 @@ class ARCQUERYHarvester(Harvester):
     """
     # the number of pages the harvester received
     pageCount = 0
+    pageCall = 1
     # the total records the harvester received
     totalCount = 0
     # the parameter "rows" and value that is passed in the request
@@ -22,55 +24,101 @@ class ARCQUERYHarvester(Harvester):
     # the number of records received in the current request
     numberOfRecordsReturned = 0
 
+    __grantList = []
+
     def harvest(self):
         self.setupdirs()
         self.updateHarvestRequest()
         self.setUpCrosswalk()
-        self.startPosition = 0
         self.data = None
-        # keep harvesting until something terminates the harvest
-        # zero records returned
-        # error occurred
-        # stopped by the registry
-        # completed
-        # set the self.numberOfRecordsReturned greater than 0 before first call
         self.numberOfRecordsReturned = 1
         while not self.errored and not self.completed and not self.stopped \
                 and self.numberOfRecordsReturned > 0:
+            self.getGrantList()
             self.getHarvestData()
             self.storeHarvestData()
+            self.pageCount += 1
             # give the server a break
             time.sleep(1)
-        # after finishing all request
-        # run crosswalk (if defined)
         self.runCrossWalk()
         self.postHarvestData()
         self.finishHarvest()
 
 
-    def getHarvestData(self):
+    def getGrantList(self):
         """
-        gets a set of 400  ARC SOLR results in JSON format
+        Using the arc grants portal  query we can obtain the identifiers all items the dataportal server provides and group them by page size
         :return:
+        :rtype:
         """
+
         if self.stopped:
             return
+       # while not self.errored and not self.completed and not self.stopped:
         request_url = self.getRequestUrl()
         getRequest = Request(request_url)
+        self.setStatus("HARVESTING")
+        # we will collect grantids from the output page by page for later processing
         try:
-            self.setStatus("HARVESTING", "getting data url:%s" %(request_url))
-            self.logger.logMessage(
-                "ARC QUERY (getHarvestData), getting data url:%s" %(request_url),
-                "DEBUG")
             self.data = getRequest.getData()
-            # find out how many result we have received
+            self.pageCall += 1
+            package = json.loads(self.data);
             self.getRecordCount()
-            # check if the harvest is completed by receiving nothing or more than the test limit
-            if self.numberOfRecordsReturned == 0 or (self.harvestInfo['mode'] == 'TEST' and self.recordCount >= myconfig.test_limit):
-                self.completed = True
+            if isinstance(package, dict):
+                i=0
+                while i < len(package['data']):
+                    self.__grantList.append(package['data'][i]['id'])
+                    i += 1
+                # check if the collection of ids is completed by receiving nothing or more than the test limit
+                if self.numberOfRecordsReturned == 0 or (
+                        self.harvestInfo['mode'] == 'TEST' and self.recordCount >= myconfig.test_limit):
+                    self.completed = True
         except Exception as e:
-                self.logger.logMessage("ERROR RECEIVING  ARC QUERY DATA, %s" % str(repr(e)), "ERROR")
+            self.handleExceptions(e)
+            return
         del getRequest
+
+
+    def getHarvestData(self):
+        """
+        gets  ARC SOLR results in JSON format for each item in the self.__grantsList (list of list per page)
+        :return:
+        """
+        self.__xml = Document()
+
+        if self.stopped:
+            return
+        baseUrl = self.harvestInfo['uri']
+        getRequest = Request(baseUrl)
+        eGrants = self.__xml.createElement('grants')
+        self.__xml.appendChild(eGrants)
+        self.listSize = len(self.__grantList)
+        self.logger.logMessage("self.listSize: (%s)" % str(self.listSize), "DEBUG")
+        for grantId in self.__grantList:
+            self.recordCount += 1
+            if self.stopped:
+                break
+            self.setStatus("HARVESTING", 'getting arc record: %s' % grantId)
+            try:
+                getRequest.setURL(baseUrl + "/" + grantId)
+                grantJson = json.loads(getRequest.getData())
+                if isinstance(grantJson, dict):
+                    eGrant = self.__xml.createElement('grant')
+                    eGrant.setAttribute('uri', grantJson['links']['self'])
+                    self.parse_element(eGrant, grantJson['data'])
+                    eGrants.appendChild(eGrant)
+                if self.recordCount >= myconfig.test_limit and self.harvestInfo['mode'] == 'TEST':
+                    break
+            except Exception as e:
+                self.errored = True
+                self.errorLog = self.errorLog + "\nERROR RECEIVING ITEM:%s, " % itemId
+                self.handleExceptions(e, terminate=False)
+                self.logger.logMessage("ERROR RECEIVING ITEM (%s/%s)" % (str(repr(e)), itemId), "ERROR")
+            # get harvestData calls storeHarvestData for each page list
+        self.data = str(self.__xml.toprettyxml(encoding='utf-8', indent=' '), 'utf-8')
+
+
+
 
 
     def getRequestUrl(self):
@@ -78,26 +126,30 @@ class ARCQUERYHarvester(Harvester):
         append the start and limit to the end of the query
         :return url:
         """
-        urlParams = 'page[number] = ' + self.pageCount + 'page[size] =' + self.rows
+        urlParams = {}
+        urlParams['page[number]'] = str(self.pageCall)
+        urlParams['page[size]'] = self.rows
 
         query = urllib.parse.urlencode(urlParams)
-        return self.harvestInfo['uri'] + query
+
+        return self.harvestInfo['uri'] + "?" + query
 
     def getRecordCount(self):
         """
         checks if the request is successful and determines the record count we received and increments the total count
         :return:
         """
+
         self.numberOfRecordsReturned = 0
         if self.stopped:
             return
         try:
             data = json.loads(self.data, strict=False)
-            self.logger.logMessage("Can we log data?" + json.dumps(data))
-            self.totalCount = int(data['hitCount'])
+            self.totalCount = int(data['meta']['total-size'])
+
             if self.totalCount > 0:
-                 # self.numberOfRecordsReturned = int(len(data['dataSets']))
-                self.pageCount += 1
+                self.numberOfRecordsReturned = int(data['meta']['actual-page-size'])
+
             self.logger.logMessage("ARC Harvester (numberOfRecordsReturned) %d of %d" % ((self.numberOfRecordsReturned * self.pageCount), self.totalCount), "DEBUG")
             # sanity check
             self.recordCount += self.numberOfRecordsReturned
--- tests/test_arc_query_harvester.py
+++ tests/test_arc_query_harvester.py
@@ -5,11 +5,12 @@ import io, os
 from mock import patch
 from utils.Request import Request
 import threading
+import numpy as np
 
 class test_arc_query_harvester(unittest.TestCase):
 
     def readTestfile(self, path):
-        f = io.open(myconfig.abs_path + '/tests/resources/test_source/arc/' + path, mode="r")
+        f = io.open(myconfig.abs_path + 'tests/resources/test_source/arc/' + path, mode="r")
         data = f.read()
         f.close()
         return data
@@ -21,15 +22,17 @@ class test_arc_query_harvester(unittest.TestCase):
         return data
 
     @patch.object(Request, 'getData')
-    def test_magda_query(self, mockGetData):
+    def test_arc_query(self, mockGetData):
         batch_id = "ARC"
-        ds_id = 9
+        ds_id = 31
         mockGetData.side_effect = [
-            self.readTestfile('grants_0.json'),
-            self.readTestfile('grants_1.json')
+           self.readTestfile('grants.json'),
+           self.readTestfile('LP190100083.json'),
+           self.readTestfile('LP190100294.json'),
+           self.readTestfile('LP190100551.json')
         ]
         harvestInfo = {}
-        harvestInfo['uri'] = 'https://dataportal.arc.gov.au/NCGP/API/grants?'
+        harvestInfo['uri'] = 'https://dataportal.arc.gov.au/NCGP/API/grants'
         harvestInfo['provider_type'] = 'ARCQUERY'
         harvestInfo['harvest_method'] = 'ARCQUERY'
         harvestInfo['data_store_path'] = myconfig.data_store_path
@@ -38,43 +41,43 @@ class test_arc_query_harvester(unittest.TestCase):
         harvestInfo['harvest_id'] = 7
         harvestInfo['batch_number'] = batch_id
         harvestInfo['advanced_harvest_mode'] = "STANDARD"
-        harvestInfo['xsl_file'] = "tests/resources/xslt/ARCAPI_json_to_rif-cs.xsl"
+        harvestInfo['xsl_file'] = myconfig.abs_path + "/tests/resources/xslt/ARCAPI_json_to_rif-cs.xsl"
         harvestInfo['mode'] = "TEST"
-        # harvestReq = JSONLDHarvester.JSONLDHarvester(harvestInfo)
-        # t = threading.Thread(name='JSONLD', target=harvestReq.harvest)
-        # t.start()
         harvester = ARCQUERYHarvester(harvestInfo)
         harvester.harvest()
 
-        tempFile = myconfig.data_store_path + str(ds_id) + os.sep + batch_id + os.sep + "1.tmp"
-        resultFile = myconfig.data_store_path + str(ds_id) + os.sep + batch_id + os.sep + "1.xml"
+        tempFile = myconfig.data_store_path + str(ds_id) + os.sep + batch_id + os.sep + "0.tmp"
+        resultFile = myconfig.data_store_path + str(ds_id) + os.sep + batch_id + os.sep + "0.xml"
         self.assertTrue(os.path.exists(tempFile))
         self.assertTrue(os.path.exists(resultFile))
         content = self.readFile(resultFile)
-       # self.assertIn('<key>data.gov.au/ds-dga-806c3770-031e-41da-99cd-9fe79f237050</key>', content)
+        self.assertIn('<key>LP190100083</key>', content)
         content = self.readFile(tempFile)
-      # self.assertIn('<identifier>ds-dga-806c3770-031e-41da-99cd-9fe79f237050</identifier>', content)
+        self.assertIn('<id>LP190100083</id>', content)
 
 
     def only_during_development_test_retry_count(self):
+        batch_id = "ARC_GRANTS"
+        ds_id = 29
         harvestInfo = {}
-        harvestInfo['uri'] = 'https://dataportal.arc.gov.au/NCGP/API/grants?'
+        harvestInfo['uri'] = 'https://dataportal.arc.gov.au/NCGP/API/grants'
         harvestInfo['provider_type'] = 'ARCQUERY'
         harvestInfo['harvest_method'] = 'ARCQUERY'
         harvestInfo['data_store_path'] = myconfig.data_store_path
         harvestInfo['response_url'] = myconfig.response_url
-        harvestInfo['data_source_id'] = '9'
+        harvestInfo['data_source_id'] = '29'
         harvestInfo['harvest_id'] = '7'
-        harvestInfo['batch_number'] = "ARC_JSON_GRANTS"
+        harvestInfo['batch_number'] = "ARC_GRANTS"
         harvestInfo['advanced_harvest_mode'] = "STANDARD"
-        harvestInfo['xsl_file'] = "tests/resources/xslt/ARCAPI_json_to_rif-cs.xsl"
+        harvestInfo['xsl_file'] = myconfig.abs_path +  "tests/resources/xslt/ARCAPI_json_to_rif-cs.xsl"
         harvestInfo['mode'] = "TEST"
-        # harvestReq = JSONLDHarvester.JSONLDHarvester(harvestInfo)
-        # t = threading.Thread(name='JSONLD', target=harvestReq.harvest)
-        # t.start()
         harvester = ARCQUERYHarvester(harvestInfo)
         harvester.harvest()
 
+        tempFile = myconfig.data_store_path + str(ds_id) + os.sep + batch_id + os.sep + "0.tmp"
+        self.assertTrue(os.path.exists(tempFile))
+        resultFile = myconfig.data_store_path + str(ds_id) + os.sep + batch_id + os.sep + "0.xml"
+        self.assertTrue(os.path.exists(resultFile))
 
 if __name__ == '__main__':
     unittest.main()
